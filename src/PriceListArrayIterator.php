18e1b58fe79e692f46aa4eeccef94453c8263734
Kadanin <kadartalek@gmail.com>
2023-02-20 09:39:34 +0300

    Mod

--- src/PriceListArrayIterator.php
+++ src/PriceListArrayIterator.php
@@ -4,6 +4,19 @@ namespace Kadartalek\Eqvanta;
 
 class PriceListArrayIterator extends \ArrayIterator implements PriceListInterface
 {
+    public function __construct(object|array $array = [], int $flags = 0)
+    {
+        $first = \reset($array);
+
+        if (!($first instanceof PriceListInterface)) {
+            $array = \array_map(static function ($v) {
+                return new PathSegment($v[0] ?? $v['length'], $v[1] ?? $v['price']);
+            }, $array);
+        }
+
+        parent::__construct($array, $flags);
+    }
+
     public function current(): PathSegmentInterface
     {
         return parent::current();
--- tests/cases/MainTest.php
+++ tests/cases/MainTest.php
@@ -3,9 +3,7 @@
 namespace tests\cases;
 
 use Kadartalek\Eqvanta\DistancePriceCalculator;
-use Kadartalek\Eqvanta\PathSegment;
-use Kadartalek\Eqvanta\PathSegmentInterface;
-use Kadartalek\Eqvanta\PriceListInterface;
+use Kadartalek\Eqvanta\PriceListArrayIterator;
 use PHPUnit\Framework\TestCase;
 
 class MainTest extends TestCase
@@ -18,18 +16,11 @@ class MainTest extends TestCase
         static::assertEquals(
             '26350.0000',
             (new DistancePriceCalculator(
-                new class (\array_map(static function ($v) {
-                    return new PathSegment($v[0], $v[1]);
-                }, [
+                new PriceListArrayIterator([
                     [100, '100'],
                     [200, '80'],
                     [1, '70'],
-                ])) extends \ArrayIterator implements PriceListInterface {
-                    public function current(): PathSegmentInterface
-                    {
-                        return parent::current();
-                    }
-                }
+                ])
             ))->run(305)
         );
     }
